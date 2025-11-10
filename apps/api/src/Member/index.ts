bb8c9f8740087c534738d2f8b19cc9d3129df78d
cDurham <47431093+cDurham@users.noreply.github.com>
2025-11-09 17:06:44 -0700

    Remove unused factory and seeder exports from Member and Retreat modules to streamline the codebase.

--- apps/api/src/Member/index.ts
+++ apps/api/src/Member/index.ts
@@ -1,6 +1,4 @@
 export * from "./member.entity";
-export * from "./member.factory";
 export * from "./member.input";
 export * from "./member.module";
 export * from "./member.resolver";
-export * from "./member.seeder";
--- apps/api/src/Retreat/index.ts
+++ apps/api/src/Retreat/index.ts
@@ -1,6 +1,4 @@
 export * from "./retreat.entity";
-export * from "./retreat.factory";
 export * from "./retreat.input";
 export * from "./retreat.module";
 export * from "./retreat.resolver";
-export * from "./retreat.seeder";
