f232bc73922756f47f0c3f1b158ae52e1251d564
cDurham <47431093+cDurham@users.noreply.github.com>
2025-11-10 22:01:41 -0700

    Update TypeScript configurations for API
    - Changed target version to ES2022 and updated module resolution in `tsconfig.base.json`, `tsconfig.app.json`, and `tsconfig.spec.json` for improved compatibility.
    - Refactored `jest.config.ts` to use `fileURLToPath` for dynamic directory resolution, enhancing test configuration reliability.

--- apps/api/jest.config.ts
+++ apps/api/jest.config.ts
@@ -1,16 +1,19 @@
-import 'reflect-metadata';
-import { join } from 'path';
+import "reflect-metadata";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
 
 const config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  rootDir: __dirname,
-  collectCoverageFrom: ['src/**/*.(t|j)s'],
-  coverageDirectory: join(__dirname, '../../coverage/api'),
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  rootDir: currentDir,
+  collectCoverageFrom: ["src/**/*.(t|j)s"],
+  coverageDirectory: join(currentDir, "../../coverage/api"),
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest'
-  }
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
 };
 
 export default config;
